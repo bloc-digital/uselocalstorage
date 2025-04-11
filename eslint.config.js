2907f8788491208d9d67c1e69f62ac0656349001
Andrew Bone <Andrew.Bone@bloc.digital>
2025-04-11 09:39:30 +0100

    🔧 update eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,50 +1,35 @@
-module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'react-app',
-    'react-app/jest',
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:prettier/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh', 'prettier'],
-  rules: {
-    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-    'padding-line-between-statements': 'warn', // require a new line between statements/functions
-    'newline-before-return': 'warn', // there should be a new line before calling return
-    'prefer-const': 'warn', // where possible const should be used
-    'no-empty': 'warn', // no block (function/if statement) should be empty
-    'no-else-return': 'warn', // else returns are not required
-    'prettier/prettier': 'warn', // code should be formatted to match the prettier spec
-    'no-async-promise-executor': 'off',
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import prettier from 'eslint-plugin-prettier'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      'prettier': prettier
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'padding-line-between-statements': 'warn', // require a new line between statements/functions
+      'newline-before-return': 'warn', // there should be a new line before calling return
+      'prefer-const': 'warn', // where possible const should be used
+      'no-empty': 'warn', // no block (function/if statement) should be empty
+      'no-else-return': 'warn', // else returns are not required
+      'prettier/prettier': 'warn', // code should be formatted to match the prettier spec
+      'no-async-promise-executor': 'off', // allow async in promise
+      '@typescript-eslint/no-unused-expressions': 'off', // allow unused expressions
+    },
   },
-};
-module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'react-app',
-    'react-app/jest',
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:prettier/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh', 'prettier'],
-  rules: {
-    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-    'padding-line-between-statements': 'warn', // require a new line between statements/functions
-    'newline-before-return': 'warn', // there should be a new line before calling return
-    'prefer-const': 'warn', // where possible const should be used
-    'no-empty': 'warn', // no block (function/if statement) should be empty
-    'no-else-return': 'warn', // else returns are not required
-    'prettier/prettier': 'warn', // code should be formatted to match the prettier spec
-    'no-async-promise-executor': 'off',
-  },
-};
+)
